3c26416001404d48bece8947cea6a329f9d53331
Nicolas de Campos Fischer <79231180+NicolasFischer2002@users.noreply.github.com>
2025-06-09 20:05:55 -0300

    New side menu

--- Tests/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ Tests/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4574a5369b502a56a2c8e6f34b25b571899cd77d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d53ee7dd77a6f1cecae4bc8ce6b41d60fd70b97")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
