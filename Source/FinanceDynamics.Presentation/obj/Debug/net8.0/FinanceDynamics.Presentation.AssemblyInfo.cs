3d53ee7dd77a6f1cecae4bc8ce6b41d60fd70b97
Nicolas de Campos Fischer <79231180+NicolasFischer2002@users.noreply.github.com>
2025-06-09 18:49:47 -0300

    New component: CardCustom

--- Source/FinanceDynamics.Presentation/obj/Debug/net8.0/FinanceDynamics.Presentation.AssemblyInfo.cs
+++ Source/FinanceDynamics.Presentation/obj/Debug/net8.0/FinanceDynamics.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceDynamics.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4574a5369b502a56a2c8e6f34b25b571899cd77d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5d2fa3c2d7405b914e7ecb1dd9ee590f8f47fc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceDynamics.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceDynamics.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
