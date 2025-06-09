b5d2fa3c2d7405b914e7ecb1dd9ee590f8f47fc5
Nicolas de Campos Fischer <79231180+NicolasFischer2002@users.noreply.github.com>
2025-06-09 14:39:18 -0300

    New AppBar + New SideBar

--- Source/FinanceDynamics.Application/obj/Release/net8.0/FinanceDynamics.Application.AssemblyInfo.cs
+++ Source/FinanceDynamics.Application/obj/Release/net8.0/FinanceDynamics.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceDynamics.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4574a5369b502a56a2c8e6f34b25b571899cd77d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceDynamics.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceDynamics.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
