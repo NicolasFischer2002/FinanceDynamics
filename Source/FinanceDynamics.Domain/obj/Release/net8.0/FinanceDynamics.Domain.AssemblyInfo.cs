0473f55a285ab457994052d39f39277020d325f4
Nicolas de Campos Fischer <79231180+NicolasFischer2002@users.noreply.github.com>
2025-06-11 20:43:52 -0300

    Styling of cards with monetary values

--- Source/FinanceDynamics.Domain/obj/Release/net8.0/FinanceDynamics.Domain.AssemblyInfo.cs
+++ Source/FinanceDynamics.Domain/obj/Release/net8.0/FinanceDynamics.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceDynamics.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d53ee7dd77a6f1cecae4bc8ce6b41d60fd70b97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8309c0ad6ff2c9ce0018cc4935fd3b7bf7558070")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceDynamics.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceDynamics.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Tests/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ Tests/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c26416001404d48bece8947cea6a329f9d53331")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8309c0ad6ff2c9ce0018cc4935fd3b7bf7558070")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
